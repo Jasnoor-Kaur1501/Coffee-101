370bfa924a0ee0a89b1ec16debd748ba118b61b5
Jasnoor Kaur <jasnoorkaurjasi@gmail.com>
2026-02-05 12:00:17 +0530

    Update station.js

--- station.js
+++ station.js
@@ -1,15 +1,15 @@
-const kettle = document.querySelector(".kettle");
-const coffee = document.querySelector(".coffee");
+const cup=document.getElementById("cup");
 
-let filled = false;
+function pour(){
+const coffee=document.createElement("div");
+coffee.style.position="absolute";
+coffee.style.bottom="0";
+coffee.style.width="100%";
+coffee.style.height="60%";
+coffee.style.background="#3b2a1f";
+cup.appendChild(coffee);
+}
 
-kettle.addEventListener("click", () => {
-  if (!filled) {
-    coffee.style.height = "70%";
-    filled = true;
-  } else {
-    coffee.style.height = "0%";
-    filled = false;
-  }
-});
-
+function steam(){
+cup.style.background="#f7efe7";
+}
